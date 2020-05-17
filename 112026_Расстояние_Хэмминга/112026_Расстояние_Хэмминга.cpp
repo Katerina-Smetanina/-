b807d1c7b201ded4735589a48ad1d87cc0b5e277
Екатерина Сметанина <kat.smetanina9@gmail.com>
2020-05-18 00:08:13 +1000

    first commit

---
+++ 112026_Расстояние_Хэмминга/112026_Расстояние_Хэмминга.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+
+int main()
+{
+	string word1, word2;
+	int sum = 0;
+	getline(cin, word1);
+	getline(cin, word2);
+
+
+	for (int i = 0; i < word1.length(); i++)
+		if (word1[i] != word2[i])
+			sum++;
+
+	cout << sum;
+}
